57deed0dadc11d42d8eb8d18987940b3f88f3c18
james <sh.james@qq.com>
2018-04-21 08:29:27 +0800

    home page

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,6 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
+<style lang="less">
 @import "./assets/base.less";
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="homePage">
-        <div>logo</div>
-        <ul>
+        <img class="logo" :src="logoUrl">
+        <ul class="nav">
             <li v-for="(item,index) in homeList" :key="index">
                 <img :src="item.SortPath">
-                {{item.SortName}}</li>
+                <div>{{item.SortName}}</div>
+            </li>
         </ul>
+        <div class="footer">{{company}}</div>
     </div>
 </template>
 
@@ -15,43 +17,39 @@ export default {
     data() {
         return {
             homeList:[],
-            //   msg: 'HelloWorld'
+            logoUrl:'',
+            company:'',
         }
     },
     created() {
         this.getMenu();
+        this.getLogin();
     },
     methods: {
         getMenu() {
             this.$http.get('/api/ProdSort/gh_6297f82da259').then(res => {
-                console.log(JSON.parse(res.data))
+                // console.log(JSON.parse(res.data))
                 let getData = JSON.parse(res.data);
                 this.homeList = getData.DataList;
-                // this.msg = res.data.msg;
-                // success callback
+            }, res => {
+                // error callback
+            });
+        },
+        getLogin() {
+            this.$http.get('/api/CustomerList?weiXinCode=gh_6297f82da259').then(res => {
+                let getData = JSON.parse(res.data);
+                // console.log(getData)
+                this.logoUrl = getData.DataList[0].LogoPath;
+                this.company = getData.DataList[0].CustName;
             }, res => {
                 // error callback
             });
         }
+
     }
 }
 </script>
 
-<style scoped lang="less">
-.homePage{
-    border: 1px solid blue;
-    height: 100%;
-    text-align: center;
-    >ul{
-        >li{
-            border: 1px solid red;
-            display: inline-block;
-            >img{
-                width: 50px;
-                height: 50px;
-            }
-        }
-    }
-}
+<style lang="less">
 
 </style>
