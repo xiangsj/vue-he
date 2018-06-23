3fa517f522470ddc5aac801e52fa09f83079e179
james <sh.james@qq.com>
2018-06-23 23:26:38 +0800

    search add active tabs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vue-he
+# vue
 
-> web for he
+> web
 
 ## Build Setup
 
@@ -19,3 +19,8 @@ npm run build --report
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+##备注：
+1. /dist 下的 文件 要放到 网站根目录
+2. 默认调的 api 为 http://120.27.163.36:5568 
+3. 若要改为 localhsot 本地配置时，不明白请联系本人
--- src/pages/search.vue
+++ src/pages/search.vue
@@ -4,27 +4,28 @@
             <img class="logo" :src="getLogoUrl()">
         </div>
         <mt-navbar class="navbar" v-model="selected">
-            <mt-tab-item id="1">车型</mt-tab-item>
-            <mt-tab-item id="2">编号</mt-tab-item>
-            <mt-tab-item id="3">Vin码</mt-tab-item>
-            <mt-tab-item :id="'tab'+item.FID" v-for="(item,index) in tabsMore" :key="index">{{item.ShowCaption}}</mt-tab-item>
+            <mt-tab-item :id="item.Name" v-for="(item,index) in tabsFixed" :key="'a'+index" v-if="item.IsShow">{{item.Name}}</mt-tab-item>
+            <!-- <mt-tab-item id="2">编号</mt-tab-item>
+            <mt-tab-item id="3">Vin码</mt-tab-item> -->
+            <mt-tab-item :id="'tab'+item.FID" v-for="(item,index) in tabsMore" :key="'b'+index">{{item.ShowCaption}}</mt-tab-item>
         </mt-navbar>
 
         <!-- tab-container -->
         <mt-tab-container v-model="selected">
-            <mt-tab-container-item id="1">
-                <search-body-1 :mSortNo="mSortNo"></search-body-1>
+            <mt-tab-container-item :id="item.Name" v-for="(item,index) in tabsFixed" :key="'a'+index" v-if="item.IsShow">
+                <!-- <search-body-1 :mSortNo="mSortNo"></search-body-1> -->
+                <component v-bind:is="'search-body-' + parseInt(index+1)"></component>  
             </mt-tab-container-item>
 
-            <mt-tab-container-item id="2">
+            <!-- <mt-tab-container-item id="2">
                 <search-body-2 :mSortNo="mSortNo"></search-body-2>
             </mt-tab-container-item>
 
             <mt-tab-container-item id="3">
                 <search-body-3 :mSortNo="mSortNo"></search-body-3>
-            </mt-tab-container-item>
+            </mt-tab-container-item> -->
 
-            <mt-tab-container-item :id="'tab'+item.FID" v-for="(item,index) in tabsMore" :key="index">
+            <mt-tab-container-item :id="'tab'+item.FID" v-for="(item,index) in tabsMore" :key="'b'+index">
                 <search-body-4 :mSortNo="mSortNo" :itemData="item"></search-body-4>
             </mt-tab-container-item>
         </mt-tab-container>
@@ -45,8 +46,9 @@ export default {
     name: 'search',
     data() {
         return {
-            selected: '1',
-            tabsMore: [],
+            selected: '',
+            tabsFixed:[],//前面常用固定项
+            tabsMore: [],//后面动态
 
             mSortNo: '',
             username: '',
@@ -60,16 +62,35 @@ export default {
         }
         this.mSortNo = this.$route.params.id;
         this.getTabs();
+        this.getTabsFixed();
         // console.log(this.mSortNo)
     },
     methods: {
         getLogoUrl(){
             return getCookie("logoUrl");
         },
+        getTabsFixed(){
+            // /api/MainSearchTitle?weiXinCode=gh_6297f82da263
+            this.$http.get('/api/MainSearchTitle', {params:{}}).then(res => {
+                // console.log(" 自定义导航 ")
+                // console.log(res.DataList)
+                this.tabsFixed = res.DataList;
+                //匹配要显示中的首项
+                let show = res.DataList.filter(function(item,index){
+                    return item.IsShow == true;
+                });
+                // console.log(show)
+                if(show.length===0){return;}
+                this.selected = show[0].Name;//首项显示
+            }, res => {
+                // error callback
+            });
+        },
         getTabs() {
             this.$http.get('/api/DesignField', {params:{}}).then(res => {
                 Indicator.close();
                 // console.log(" 自定义导航 ")
+                // console.log(res.DataList)
                 this.tabsMore = res.DataList;
             }, res => {
                 // error callback
